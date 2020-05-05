583e61ceb0dc74d163563490f91dc0af36cb9c58
Anders Stromberg <coltlaboy@yahoo.se>
2020-05-05 08:27:45 +0200

    adding google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,8 +35,18 @@ module.exports = {
               maxWidth: 1200,
             },
           },
-        ]
+        ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: "swap",
+      }
+    }
   ],
 }
